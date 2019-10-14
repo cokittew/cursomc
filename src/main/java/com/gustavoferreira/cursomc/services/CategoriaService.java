3b8c0b36d89680acaf749f0f3b3f90ac8a8165fe
cokittew <xondolymp13@hotmail.com>
2019-10-14 12:39:46 -0300

    Deletando uma Categoria, Exceção personalizada

--- src/main/java/com/gustavoferreira/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavoferreira/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.gustavoferreira.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.gustavoferreira.cursomc.domain.Categoria;
 import com.gustavoferreira.cursomc.repositories.CategoriaRepository;
+import com.gustavoferreira.cursomc.services.exception.DataIntegrityException;
 import com.gustavoferreira.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,13 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos!");
+		}
+	}
 	
 }
